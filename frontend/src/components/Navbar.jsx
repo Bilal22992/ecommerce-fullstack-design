f45031ed1cdc3c616c58911d41b9bfc30d46c495
bilal22992 <haiderbilal22992@gmail.com>
2025-12-04 22:36:09 +0500

    worked on Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,15 +1,48 @@
 
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import "../styles/Navbar.css"
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Navbar ()
 {
 
     return(
-        <div>
-            <Link to="/" >Home</Link> 
+        <div className="navbar-main">
+            {/* <p><Link to="/" >Home</Link></p> 
             <Link to="/cart">Cart</Link>
             <Link to="/productListing">productListings</Link>
-            <Link to="/productDetails">productDetails</Link>
+            <Link to="/productDetails">productDetails</Link> */}
+            <div className="navbar-left">
+                <ul>
+                    <li>
+                        <div className="navbar-allcategory">
+                        <i className="fa-solid fa-bars"></i>
+                        <p>All Categories</p>
+                        </div>
+                        </li>
+                    <li>Hot Offers</li>
+                    <li>Gift Boxes</li>
+                    <li>Projects</li>
+                    <li>Menu Item</li>
+                    <li><select>
+                        <option>
+                        Help
+                        </option>
+                        <option> Customer Service</option>
+                        </select></li>
+                </ul>   
+            </div>
+            <div className="navbar-right">
+                <ul>
+                  <li><select>
+                    <option>English,USD</option>
+                    <option>English,GB</option>
+                    <option>Spanish,ESP</option>
+                  
+                  </select></li>
+                    <li></li>
+                </ul>
+            </div>
             
         </div>
     )
