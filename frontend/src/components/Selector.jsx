fd2bef7051daa890baff148660140b26feada9be
bilal22992 <haiderbilal22992@gmail.com>
2025-12-15 08:14:12 +0500

    Selector initially completed

--- frontend/src/components/Selector.jsx
+++ frontend/src/components/Selector.jsx
@@ -2,74 +2,76 @@ import "../styles/Selector.css"
 function Selector ()
 {
     return(
-
-        <div className="main">
+    <div className="selector-outerbox">
+        <div className="selector-main">
             <div>
                
             </div>
-             <div> 
+             <div className="selector-checkboxes-container"> 
                 <p><strong>Brands</strong></p>
-                    <input type="checkbox" id="apple" name="brand" value="Samsung"/>
-                    <label for="Samsung">Samsung</label>
+                  <div className="selector-checkbox">
+                    <input type="checkbox" id="Samsung" name="brand" value="Samsung"/>
+                    <label htmlFor="Samsung">Samsung</label>
+                </div>
+                <div className="selector-checkbox">
                     <input type="checkbox" id="Apple" name="brand" value="Apple"/>
-                    <label for="Apple">Apple</label>
+                    <label htmlFor="Apple">Apple</label>
+                </div>
+                <div className="selector-checkbox">
                     <input type="checkbox" id="Huawei" name="brand" value="Huawei"/>
-                    <label for="apple">Huawei</label>
+                    <label htmlFor="Huawei">Huawei</label>
+                </div>
+                <div className="selector-checkbox">
                     <input type="checkbox" id="Poco" name="brand" value="Poco"/>
-                    <label for="apple">Poco</label>
+                    <label htmlFor="Poco">Poco</label>
+                </div>
+                <div className="selector-checkbox">
                     <input type="checkbox" id="Lenovo" name="brand" value="Lenovo"/>
-                    <label for="apple">Lenovo</label>
+                    <label htmlFor="Lenovo">Lenovo</label>
+                </div>
 
             </div>
-             <div>
+             {/* <div className="selector-checkboxes-container">
                     <p><strong>Features</strong></p>
+                    <div className="selector-checkbox">
                     <input type="checkbox" id="apple" name="brand" value="Samsung"/>
-                    <label for="Samsung">Samsung</label>
+                    <label htmlFor="Samsung">Samsung</label>
+                    </div>
+                    <div className="selector-checkbox">
                     <input type="checkbox" id="Apple" name="brand" value="Apple"/>
-                    <label for="Apple">Apple</label>
+                    
+                    <label htmlFor="Apple">Apple</label>
+                    </div>
+                    <div className="selector-checkbox">
                     <input type="checkbox" id="Huawei" name="brand" value="Huawei"/>
-                    <label for="apple">Huawei</label>
+                    
+                    <label htmlFor="apple">Huawei</label>
+                    </div>
+                    <div className="selector-checkbox">
                     <input type="checkbox" id="Poco" name="brand" value="Poco"/>
-                    <label for="apple">Poco</label>
+                    
+                    
+                    <label htmlFor="apple">Poco</label>
+                    </div>
+                    <div className="selector-checkbox">
                     <input type="checkbox" id="Lenovo" name="brand" value="Lenovo"/>
-                    <label for="apple">Lenovo</label>
-            </div>
-             <div>
-<div class="price-range-container">
-    <div class="price-range-header">
-        Price range
-        </div>
-
-    <div class="price-range-slider">
-        <input type="range" min="0" max="999999" value="[min_selected_value]"/>
-        <input type="range" min="0" max="999999" value="[max_selected_value]"/>
-    </div>
-
-    <div class="price-range-inputs">
-        <div class="price-input-group">
-            <label for="min-price">Min</label>
-            <input type="number" id="min-price" value="0"/>
-        </div>
-
-        <div class="price-input-group">
-            <label for="max-price">Max</label>
-            <input type="number" id="max-price" value="999999"/>
-        </div>
-    </div>
-
-    <button class="apply-button">Apply</button>
-</div>
-            </div>
+                    <label htmlFor="apple">Lenovo</label>
+                    </div>
+                    
+                    
+            </div> */}
              <div>
 
+    <button className="apply-button">Apply</button>
             </div>
-             <div>
-
             </div>
+            
+             
 
 
 
         </div>
+        
     )
 }
 
